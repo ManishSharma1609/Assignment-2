90a4bd1c7acbb45050d2731932ea7501f7457df0
ManishSharma1609 <104347990+ManishSharma1609@users.noreply.github.com>
2022-05-24 13:03:10 +0530

    Update Python assignment  2.py

--- Python assignment  2.py
+++ Python assignment  2.py
@@ -80,7 +80,7 @@ print('Q U E S T I O N  5')
 a = int(input("Please enter the length of side 1 : "))
 b = int(input("Please enter the length of side 2 : "))
 c = int(input("Please enter the length of side 3 : "))
-while ((a+b)>c) or ((a+c)>b) or ((b+c)>a):
+while ((a+b)>c) and ((a+c)>b) and ((b+c)>a):
     print("Yes")
     break
 while ((a+b)<c) or ((a+c)<b) or ((b+c)<a):
